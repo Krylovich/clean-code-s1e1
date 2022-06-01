17b52b3d89f578c18c4c027c8a3c3eecd1d75838
Krylovich <mariya.wings@gmail.com>
2022-06-01 15:08:20 +0300

    refactor: add BEM (1.3 html-and-css-extended.md)

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     delButton.className="del";
     delButtonImg.src='./remove.svg';
+    delButtonImg.className="del-img";
     delButton.appendChild(delButtonImg);
 
 
@@ -85,11 +86,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("complete-task__mode");
+    //If class of the parent is .complete-task__mode
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .complete-task__mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +99,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .complete-task__mode on the parent.
+    listItem.classList.toggle("complete-task__mode");
 };
 
 
